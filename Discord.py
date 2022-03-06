e8dc0dc0ebf4a34f2f939b4897d0a922a9179be7
BenRider45 <benrider556@gmail.com>
2022-03-06 18:57:27 -0500

    Added Blacklist command, updated all other commands

--- Discord.py
+++ Discord.py
@@ -1,4 +1,5 @@
 import random
+from unittest.mock import Mock
 import discord
 from discord.channel import DMChannel
 from discord.ext import commands
@@ -13,9 +14,28 @@ with open('Token.txt') as f:
     TOKEN = f.readline()
 client.remove_command('help')
 
+def BlackList(ID):
+    with open('BlackList.txt') as f:
+        lines= f.readlines()
+    for line in lines:
+        line= int(line)
+
+        if line=="": 
+            continue
+        else:
+            if int(line)==ID :
+                return True
+
+            
+    return False
+
+
 @client.event  
 async def on_ready():
+    activity = discord.Game(name="Just Swaggin || (;help for more!)", type=2)
+    await client.change_presence(status=discord.Status.online, activity=activity)
     print("Bot is ready.")
+
 
 @client.event
 async def on_guild_join(Guild):  
@@ -34,14 +54,14 @@ async def on_member_join(member):
 
 
 @client.event
-async def on_member_remove(member,ctx):
+async def on_member_remove(ctx,member):
     await ctx.send(f'{member} has left a server')
 
 
 
 @client.command()
 async def help(ctx):
-    with open("helpMessage.txt","r") as file:
+    with open("README.md","r") as file:
         fileCont= file.read()
     embed = discord.Embed(description = fileCont, color=0xE74C3C)
     await ctx.author.send(embed=embed)
@@ -51,10 +71,43 @@ async def ping(ctx):
      await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
 @client.command()
-async def mock(ctx):
-    
+async def blacklist(ctx):
+    if BlackList(ctx.message.author.id):
+        await ctx.send("Nah")
+        return
+    with open("AdminPass.txt","r") as f:
+        PASS=f.read()
     SpaceLoc= ctx.message.content.find(" ")
     User= ctx.message.content[SpaceLoc+1:]
+    embed = discord.Embed(description = "Enter super secret password:", color=0xE74C3C)
+    await ctx.author.send(embed=embed)
+    
+    def check(m):
+        print(m.channel.type)
+
+        return m.content == PASS and str(m.channel.type) == "private"
+
+    msg = await client.wait_for("message", check=check)
+    USR = discord.utils.get(ctx.guild.members, display_name=User)
+    with open("BlackList.txt",'w') as f:
+        f.writeline(str(USR.id))
+    await ctx.send(f"User <@%s> has been blacklisted from using the BDB!"%(USR.id))
+
+@client.command()
+async def mock(ctx):
+    if BlackList(ctx.message.author.id):
+        await ctx.send("Nah")
+        return ctx.message.author.id
+    
+    origUser=ctx.message.author.display_name 
+    SpaceLoc= ctx.message.content.find(" ")
+    User= ctx.message.content[SpaceLoc+1:]
+    if User.upper()=="BDB":
+        MockBot=True
+        await ctx.send("Nice one chuckle nuts")
+        User = ctx.message.author.display_name
+        
+    User=User.lower()
     
     async for message in ctx.channel.history(limit=1):
         fetchMessage=message
@@ -66,13 +119,11 @@ async def mock(ctx):
             await ctx.send("Cannot find user message!")
             return
         
-        if("https://" in fetchMessage.content or len(fetchMessage.content)==0) or fetchMessage.author.display_name!= User:
+        if("https://" in fetchMessage.content or len(fetchMessage.content)==0) or fetchMessage.author.display_name.lower()!= User:
             continue
         else:
             break
     
-
-
     i=1
     output=""
     for char in fetchMessage.content:
@@ -83,26 +134,44 @@ async def mock(ctx):
         i*=-1
 
     await ctx.send(output,reference=fetchMessage)
+    if MockBot:
+        await ctx.send("==insult "+User)
 
 @client.command()
 async def luigi(ctx):
-    while True:
-        member=random.choice(ctx.guild.members)
-        if member.bot==False:  
-            break
+    if BlackList(ctx.message.author.id):
+        await ctx.send("Nah")
+        return
+    SpaceLoc= ctx.message.content.find(" ")
+    User= ctx.message.content[SpaceLoc+1:]
+
+    if User==';luigi':
+        while True:
+            member=random.choice(ctx.guild.members)
+            if member.bot==False:  
+                break
+    else:
+        member = discord.utils.get(ctx.guild.members, display_name=User)
+
     
+
     with open("luigi.txt","r") as file:
         msg1= file.read()
-    msg2="Use the command, "";Luigi"" to totally Luigi someone random in your server! "
+    msg2="Get Luigied!"
     embed = discord.Embed(description = msg2, color=0xE74C3C)    
-    await member.send(msg1)
-    await member.send(embed=embed)
-    await ctx.send("Someone just got Luigied!")
+    try:
+        await member.send(msg1)
+        await member.send(embed=embed)
+        await ctx.send("<@%s> just got Luigied!"%(member.id))
+    except discord.errors.Forbidden:
+        embed = discord.Embed(description = "ERROR: User does not except dms :(", color=0xE74C3C)    
+        await ctx.send(embed=embed)
 
 @client.command()
 async def join(ctx):   
+    
     if(ctx.author.voice):
-        channel = ctx.message.author.voice.channel
+        channel = ctx.medssage.author.voice.channel
         await channel.connect()
     else:
         await ctx.send("You have to be in a channel first silly!")
